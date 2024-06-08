124e004bbceb01a1bf59426624c50a818971a89a
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-08 20:51:48 +0200

    Blog project

---
+++ 1-Blog_Project_06_24/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '1-Blog_Project_06_24';
+}
