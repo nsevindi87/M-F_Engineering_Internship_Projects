78b33be99e36e81851a0a4403d1ecc70e623554b
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-15 12:31:50 +0200

    components

--- 2-Students-CRUD/src/app/app-routing.module.ts
+++ 2-Students-CRUD/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomePageComponent, title:"Home page"},
+  {path:"about-us",component:AboutPageComponent, title:"About Us"},
+  {path:"contact-us",component:ContactPageComponent, title:"Contact Us"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- 2-Students-CRUD/src/app/app.module.ts
+++ 2-Students-CRUD/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
+import { NavbarComponent } from './Pages/Partials/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
