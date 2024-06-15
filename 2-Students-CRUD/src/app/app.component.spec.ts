eb377c22aea1a5c1c3870ced323a398e2faa58d2
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-15 11:48:03 +0200

    new project

--- 2-Students-CRUD/src/app/app.component.spec.ts
+++ 2-Students-CRUD/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '2-Students_CRUD'`, () => {
+  it(`should have the '2-Students-CRUD' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('2-Students_CRUD');
+    expect(app.title).toEqual('2-Students-CRUD');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('2-Students_CRUD app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 2-Students-CRUD');
   });
 });
