8c109fc4829c33679ede52e2d4a3237f448361aa
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-18 00:26:19 +0200

    model module

---
+++ 3-ShopApp/src/app/model/category.repository.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnInit } from "@angular/core";
+import { RestService } from './rest.service';
+import { Category } from './category.model';
+
+@Injectable()
+
+export class CategoryRepository implements OnInit{
+    private categories: Category[] = [];
+
+    constructor(private restService: RestService){}
+
+    ngOnInit(){
+        this.restService.getCategories().subscribe(categories=>this.categories = categories)
+    }
+
+    getProduct(id:number):Category | undefined{
+        return this.categories.find(i => i.id === id);
+    }
+}
---
+++ 3-ShopApp/src/app/shop/shop.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    imports:[ModelModule],
+    providers:[]
+})
+
+export class ModelModule{}
