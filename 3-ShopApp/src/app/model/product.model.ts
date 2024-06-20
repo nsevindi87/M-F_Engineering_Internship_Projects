c372b6862c4ab48e24928a49e50eea0877a4d899
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-20 10:45:07 +0200

    add-delete-update functions

--- 3-ShopApp/src/app/model/product.model.ts
+++ 3-ShopApp/src/app/model/product.model.ts
@@ -1,8 +1,8 @@
 export class Product {
     constructor(
-        public id?:number,
-        public name?:string,
-        public price?:number,
+        public id:number,
+        public name:string,
+        public price:number,
         public imageUrl?:string,
         public description?:string,
         public category?:string
