c4028ee4144c8090203668591dcb5a6b005957f9
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-20 19:06:57 +0200

    admin page

--- 3-ShopApp/src/app/admin/products/product-list/product-list.component.ts
+++ 3-ShopApp/src/app/admin/products/product-list/product-list.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductsListComponent {
+export class ProductListComponent {
 
 }
