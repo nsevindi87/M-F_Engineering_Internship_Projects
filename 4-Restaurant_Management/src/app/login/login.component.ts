49947a4495a7060e10faa9260ba12fa3708ed9df
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-25 13:59:28 +0200

    login-signup

--- 4-Restaurant_Management/src/app/login/login.component.ts
+++ 4-Restaurant_Management/src/app/login/login.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formValue!:FormGroup;
+
+  constructor(private fb:FormBuilder, private http:HttpClient, private router:Router){}
+
+  ngOnInit(): void {
+    this.formValue = this.fb.group({
+      email:["", Validators.required],
+      password:["", Validators.required]
+    })
+  }
+
+  login(){
+    this.http.get<any>("http://localhost:3000/signup").subscribe(res=>{
+      const user = res.find((u:any)=>
+        u.email === this.formValue.value.email && 
+        u.password === this.formValue.value.password
+      )
+      
+      if(user){
+          alert("Successfully logged in")
+          this.router.navigate(['restaurant'])
+        }else{
+          alert("User not found with these credentials")
+        }
+      }
+      ,err=>{
+        alert("Something went wrong")
+    })
+  }
 
 }
--- 4-Restaurant_Management/src/app/signup/signup.component.ts
+++ 4-Restaurant_Management/src/app/signup/signup.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+  myForm!:FormGroup;
 
+  constructor(private fb:FormBuilder, private http:HttpClient, private router:Router){}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      name:["", Validators.required],
+      email:["", Validators.required],
+      mobile:["", Validators.required],
+      password:["", Validators.required],
+    })
+  }
+
+  signup(){
+    this.http.post<any>("http://localhost:3000/signup", this.myForm.value).subscribe(res=>{
+      alert("You are register Successfully!");
+      this.myForm.reset();
+      this.router.navigate(['login'])
+    },error=>{
+      alert("Something went wrong!!!")
+    })
+  }
 }
