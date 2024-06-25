e62e3ecaf7716a7abbb3263191a9d64252ff1303
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-25 11:39:26 +0200

    restaurant management system

---
+++ 4-Restaurant_Management/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<router-outlet></rooter-outlet>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '4-Restaurant_Management';
+}
