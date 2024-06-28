a4ed00dfdb91af27a770ed030857eac3891ea45f
Nizami Sevindi <nsevindi87@gmail.com>
2024-06-28 16:11:17 +0200

    formbuilder

--- 5-Forms_and_Data_Flow/src/app/form-builder/form-builder.component.ts
+++ 5-Forms_and_Data_Flow/src/app/form-builder/form-builder.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-builder',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-builder.component.css']
 })
 export class FormBuilderComponent {
+  constructor(private fb:FormBuilder){}
 
+  myFbForm = this.fb.group({
+    firstname: ["",Validators.required],
+    lastname:[""],
+    address:this.fb.group({
+      street:[""],
+      city:[""]
+    })
+  })
+
+  handleNewSubmit(){
+    console.log(this.myFbForm.value);
+    
+  }
 }
